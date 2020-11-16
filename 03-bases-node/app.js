c0b874bbe9ee5e6ba0c32a5827c49d12fb586ee6
rubensantiago24 <cityrap24444@gmail.com>
2020-11-15 19:24:02 -0500

    fix: creacion de nuevo codigo y finalizacion de parte 3

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,36 +1,33 @@
-const argv = require('yargs')
-    .command('Listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
 
 
-// const { command } = require('yargs');
+switch (comando) {
 
-// Base al multiplicar manualmente.
-// let base = '5';
-// console.log(process.argv);
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
 
-// let argv2 = process.argv;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: `, colors.green(archivo)))
+            .catch(e => console.log(e));
+        break;
 
-// console.log('Limite', argv.limite);
+    default:
+        console.log('Comando no reconocido');
+
+}
+
+
+
+// console.log(argv.base);
+
+
 
 // let parametro = argv[2];
 // let base = parametro.split('=')[1]
-
-
-// crearArchivo(base)
-//     .then(archivo => console.log(`Archivo creado ${archivo}`))
-//     .catch(err => console.log(err))
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,27 +1,51 @@
+// requireds
 const fs = require('fs');
+const colors = require('colors');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+
+    console.log('=================='.red);
+    console.log(`tabla de ${ base }`.green);
+    console.log('=================='.red);
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`)
+    }
+
+
+}
+
+
+let crearArchivo = (base, limite = 10) => {
+
     return new Promise((resolve, reject) => {
 
         if (!Number(base)) {
-            reject(`El valor introducido ${base} no es un número.`)
-            return
+            reject(`El valor introducido ${ base } no es un número`);
+            return;
         }
+
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
-            data += `${base} * ${i} = ${base * i}\n`;
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
         }
 
-        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
+
             if (err)
-                reject(err);
+                reject(err)
             else
-                resolve(`tabla-${base}.txt`)
+                resolve(`tabla-${ base }-al-${ limite }.txt`);
+
         });
-    })
+
+    });
+
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
