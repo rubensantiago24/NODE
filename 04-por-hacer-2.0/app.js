43d8853cc655b6391e3adca7016c3c71845cdce6
rubensantiago24 <cityrap24444@gmail.com>
2021-02-21 13:27:54 -0500

    Inicio de nueva seccion 4-por-hacer-2.0

---
+++ 04-por-hacer-2.0/app.js
@@ -0,0 +1,12 @@
+const {crearArchivo} = require('./helpers/multiplicar');
+
+
+console.clear();
+
+
+const base = 3;
+
+
+crearArchivo(base)
+    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+    .catch( err => console.log(err) );
